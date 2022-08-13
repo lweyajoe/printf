5ea8b10ec410884709056c4b15f6899922563db7
Joseph Lweya <84285814+lweyajoe@users.noreply.github.com>
2022-08-13 07:58:44 +0300

    Update main.h

--- main.h
+++ main.h
@@ -1,36 +1,24 @@
-#ifndef MAIN_H
-#define MAIN_H
-
+#ifndef our_printf
+#define our_printf
+#include <stdio.h>
 #include <stdarg.h>
-#include <stdlib.h>
-
 /**
- * struct printer - structure for printing to stdout, given a specifier
- * @specifier: the specifier recognized by this printer (without the %)
- * @run: the function to run when this printer is invoked
+ * struct specifier - struct specifier
+ * @valid: the valid character.
+ * @f: the functions associated.
+ *
  */
-typedef struct printer
+typedef struct specifier
 {
-	char *specifier;
-	int (*run)(va_list);
-} printer;
-
-printer _get_printer(const char *specifier);
-int _putchar(char c);
-int print_c(va_list);
-int print_s(va_list);
-int print_d(va_list);
-int print_i(va_list);
-char *itob(va_list list);
-int print_percent(va_list);
+	char *valid;
+	int (*f)(va_list);
+} spec;
 int _printf(const char *format, ...);
-int _print_char(va_list);
-int _print_str(va_list);
-int _print_decimal(va_list);
-int _print_int(va_list);
-int _print_binary(va_list);
-int _print_str_nonprintable(va_list);
-int _print_str_reverse(va_list);
-int _print_rot13(va_list);
-
+int print_c(va_list args);
+int print_s(va_list args);
+int print_d(va_list args);
+int print_i(va_list args);
+int _putchar(char c);
+int print_percent(va_list args);
+int (*get_func(char x))(va_list args);
 #endif
